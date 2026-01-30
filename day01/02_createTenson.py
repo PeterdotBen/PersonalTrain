783b309dd622a9007be756b8a3805e8037b35ad1
XIben <14567765@qq.com>
2026-01-30 18:05:54 +0800

    Tensor type v2

--- day01/02_createTenson.py
+++ day01/02_createTenson.py
@@ -11,3 +11,16 @@ data = torch.Tensor([[10]])
 print(data)
 data = torch.Tensor([10, 20])
 print(data)
+
+# 1.创建2行3列, dtype 为 int32的张量
+data = torch.IntTensor(2, 3)
+print(data)
+# 2.注意:如果传递列表,则创建包含指定元素的张量
+data = torch.IntTensor([2.5,3.3])
+print(data)
+# 其他类型
+data = torch.LongTensor()
+data2 = torch.ShortTensor()
+data3 = torch.FloatTensor()
+data4 = torch.DoubleTensor()
+print(data, data2, data3, data4)
