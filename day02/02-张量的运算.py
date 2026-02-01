3a53cf25a13cc201d283146cd2feb016fc48150b
XIben <14567765@qq.com>
2026-02-01 21:23:31 +0800

    第二天tensor的基本运算+梯度下降

--- day02/02-张量的运算.py
+++ day02/02-张量的运算.py
@@ -2,7 +2,7 @@ import torch
 
 def dm01():
     t1 = torch.randint(0, 10, (3, 4, 5))
-    print(f"t1:{t1}, shape:{t1.shape}, dtype:{t1.dtype}")
+    print(f"t1:\n{t1}, shape:{t1.shape}, dtype:{t1.dtype}")
 
     # 演示sum ()mean(),max(),min()
     # t2 = t1.sum()
@@ -13,9 +13,9 @@ def dm01():
     # t2 = t1.max()
     # print(f"t2:{t2}, shape:{t2.shape}, dtype:{t2.dtype}")
     t2 = t1.min()
-    print(f"t2:{t2}, shape:{t2.shape}, dtype:{t2.dtype}")
-    print(f"t1.min(dim=0):{t1.min(dim=0)}, shape:{t1.min(dim=0)[0].shape}")
-    print(f"t1.min(dim=0):{t1.min(dim=1)}, shape:{t1.min(dim=1)[0].shape}")
+    print(f"t2:\n{t2}, shape:{t2.shape}, dtype:{t2.dtype}")
+    print(f"t1.min(dim=0):\n{t1.min(dim=0)}, shape:{t1.min(dim=0)[0].shape}")
+    print(f"t1.min(dim=0):\n{t1.min(dim=1)}, shape:{t1.min(dim=1)[0].shape}")
 
 
 if __name__ == '__main__':
